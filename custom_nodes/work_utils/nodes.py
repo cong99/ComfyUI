6abbc0b98140504ca733e29851bf3334e1097490
cong99 <cong9466@gmail.com>
2024-05-19 17:56:27 +0800

    feat: add node load_file

--- custom_nodes/work_utils/nodes.py
+++ custom_nodes/work_utils/nodes.py
@@ -1,36 +1,51 @@
+import shutil
+
 import folder_paths
 import os
 import hashlib
 import json
 
 
+
 class LoadFile:
     '''
-    文件加载器，文件上传到临时目录处理
+    文件加载器，文件上传到输入目录
     '''
     CATEGORY = "work_utils/input"
+
+    # 常量，使用本地文件
+    USE_LOCAL_FILE = "USE_LOCAL_FILE"
 
     @classmethod
     def INPUT_TYPES(s):
         input_dir = folder_paths.get_input_directory()
-        files = [f for f in os.listdir(input_dir) if os.path.isfile(
-            os.path.join(input_dir, f))]
-        return {"required":
-            {
-                "file": (sorted(files),),
+        files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
+        return {
+            "required": {
+                "file": ([s.USE_LOCAL_FILE] + sorted(files), {"file_upload": True, "accept": "*"}),
+            },
+            "optional": {
+                "local_file": ("STRING", {"multiline": True, "default": ""}),
             },
         }
 
-    RETURN_TYPES = ("DIR",)
+    RETURN_TYPES = ("FILE",)
 
     FUNCTION = "load_file"
 
-    def load_file(self, file):
-        file_path = folder_paths.get_annotated_filepath(file)
+    def load_file(self, file, local_file):
+        if file == LoadFile.USE_LOCAL_FILE:
+            file_path = local_file
+        else:
+            file_path = folder_paths.get_annotated_filepath(file)
+        if not os.path.isfile(file_path):
+            raise Exception(f"非法文件:{file_path}")
         return (file_path,)
 
     @classmethod
     def IS_CHANGED(s, file):
+        if file == s.USE_LOCAL_FILE:
+            return s.USE_LOCAL_FILE
         file_path = folder_paths.get_annotated_filepath(file)
         m = hashlib.sha256()
         with open(file_path, 'rb') as f:
@@ -39,10 +54,12 @@ class LoadFile:
 
     @classmethod
     def VALIDATE_INPUTS(s, file):
+        if file == LoadFile.USE_LOCAL_FILE:
+            return True
         if not folder_paths.exists_annotated_filepath(file):
-            return "Invalid file: {}".format(file)
-
+            return f"非法文件:{file}"
         return True
+
 
 
 class HttpRequest:
@@ -77,7 +94,38 @@ class UnzipFile:
     '''
     解压文件
     '''
-    pass
+    CATEGORY = "work_utils/handler"
+
+    # 支持的压缩包类型
+    SUPPORT_TYPES = ("zip",)
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "file": ("FILE",),
+            },
+            "optional": {
+                "output_dir": ("STRING", {"multiline": True, "default": ""}),
+            }
+        }
+
+    RETURN_TYPES = ("DIR",)
+
+    FUNCTION = "unzip"
+
+    def unzip(self, file, output_dir):
+        if not os.path.isfile(file) or not file.split('.')[-1] in UnzipFile.SUPPORT_TYPES:
+            raise Exception(f"文件不存在或者压缩类型不支持:{file}")
+        if len(output_dir) == 0:
+           output_dir = folder_paths.get_input_directory()
+        output_dir = os.path.join(output_dir, os.path.splitext(os.path.basename(file))[0])
+        shutil.unpack_archive(file, output_dir)
+        return (output_dir,)
+
+    @classmethod
+    def IS_CHANGED(s, file,):
+        return file
 
 
 class TextExtract:
@@ -160,17 +208,21 @@ class TextOutput:
         })
         print('output,', dir, data)
         # output的节点输出一定要是ui
-        return {"ui": data }
+        return {"ui": data}
 
 
 NODE_CLASS_MAPPINGS = {
     "LoadFile": LoadFile,
+
+    "UnzipFile": UnzipFile,
+
     "HttpRequest": HttpRequest,
     "TextOutput": TextOutput,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "LoadFile": "LoadFile",
+    "UnzipFile": "UnzipFile",
     "HttpRequest": "HttpRequest",
     "TextOutput": "TextOutput"
 }
--- custom_nodes/work_utils/web/public/plugin.js
+++ custom_nodes/work_utils/web/public/plugin.js
@@ -1,4 +1,5 @@
 import { app } from '/scripts/app.js'
+import { api } from '/scripts/api.js'
 
 // 颜色参考，https://vuetifyjs.com/en/styles/colors/#material-colors
 const COLORS = {
@@ -6,38 +7,178 @@ const COLORS = {
   DIR: '#673AB7',
 }
 
+class Logger {
+  constructor(name) {
+    this.name = name
+  }
+
+  info(...msgs) {
+    console.log(`[${this.name}] [info]`, ...msgs)
+  }
+
+  debug(...msgs) {
+    console.log(`[${this.name}] [debug]`, ...msgs)
+  }
+
+  error(...msgs) {
+    console.log(`[${this.name}] [error]`, ...msgs)
+  }
+}
+
+const logger = new Logger('WorkUtils')
+
 // 节点颜色
 app.registerExtension({
   name: 'WorkUtils.Colors',
   async init(app) {
-    console.log('[WorkUtils] 导入脚本、样式')
+    logger.info('导入脚本、样式')
     const script = document.createElement('script')
     script.type = 'text/javascript'
     script.src = '/extensions/work_utils/index.js'
     document.head.appendChild(script)
 
     const link = document.createElement('link')
-    link.type = "text/css";
-    link.rel = "stylesheet";
+    link.type = 'text/css'
+    link.rel = 'stylesheet'
     link.href = '/extensions/work_utils/index.css'
     document.head.appendChild(link)
   },
   async setup(app) {
-    console.log('[WorkUtils] 设置颜色')
+    logger.info('设置颜色')
     Object.assign(app.canvas.default_connection_color_byType, COLORS)
     Object.assign(LGraphCanvas.link_type_colors, COLORS)
   },
-  // async getCustomWidgets(app) {
-  //   console.log('[logging]', 'provide custom widgets')
-  //   return {
-  //     JSON: (node, inputName, inputData, app) => {
-  //       console.log(getCustomWidgets,,,,,,,,,,,,,,,,', inputName, inputData)
-  //       return {
-  //         widget: node.addWidget('toggle', inputName, false, () => {}, {on: '开', off: '关'}),
-  //       }
-  //     },
-  //   }
-  // },
+  async getCustomWidgets(app) {
+    logger.info('加载自定义Widget')
+    return {
+      FILEUPLOAD(node, inputName, inputData, app) {
+        const attrs = inputData[1] || {}
+        const accept = attrs.accept || '*'
+        const fileWidget = node.widgets.find((w) => w.name === attrs.widget)
+        if (!fileWidget) {
+          logger.error('未找到关联的widget', attrs.widget)
+          return
+        }
+        let uploadWidget
+
+        const default_value = fileWidget.value
+        Object.defineProperty(fileWidget, 'value', {
+          set: function (value) {
+            this._real_value = value
+          },
+
+          get: function () {
+            let value = ''
+            if (this._real_value) {
+              value = this._real_value
+            } else {
+              return default_value
+            }
+
+            if (value.filename) {
+              let real_value = value
+              value = ''
+              if (real_value.subfolder) {
+                value = real_value.subfolder + '/'
+              }
+
+              value += real_value.filename
+
+              if (real_value.type && real_value.type !== 'input') value += ` [${real_value.type}]`
+            }
+            return value
+          },
+        })
+
+        async function uploadFile(file, updateNode, pasted = false) {
+          try {
+            // Wrap file in formdata so it includes filename
+            const body = new FormData()
+            body.append('image', file)
+            if (pasted) body.append('subfolder', 'pasted')
+            const resp = await api.fetchApi('/upload/image', {
+              method: 'POST',
+              body,
+            })
+
+            if (resp.status === 200) {
+              const data = await resp.json()
+              // Add the file to the dropdown list and update the widget value
+              let path = data.name
+              if (data.subfolder) path = data.subfolder + '/' + path
+
+              if (!fileWidget.options.values.includes(path)) {
+                fileWidget.options.values.push(path)
+              }
+
+              if (updateNode) {
+                fileWidget.value = path
+              }
+            } else {
+              alert(resp.status + ' - ' + resp.statusText)
+            }
+          } catch (error) {
+            alert(error)
+          }
+        }
+
+        const fileInput = document.createElement('input')
+        Object.assign(fileInput, {
+          type: 'file',
+          accept,
+          style: 'display: none',
+          onchange: async () => {
+            if (fileInput.files.length) {
+              await uploadFile(fileInput.files[0], true)
+            }
+          },
+        })
+        document.body.append(fileInput)
+
+        // Create the button widget for selecting the files
+        uploadWidget = node.addWidget('button', inputName, 'choose_file_to_upload', () => {
+          fileInput.click()
+        })
+        uploadWidget.label = 'choose file to upload'
+        uploadWidget.serialize = false
+
+        // Add handler to check if an image is being dragged over our node
+        node.onDragOver = function (e) {
+          if (e.dataTransfer && e.dataTransfer.items) {
+            const image = [...e.dataTransfer.items].find((f) => f.kind === 'file')
+            return !!image
+          }
+          return false
+        }
+
+        node.onDragDrop = function (e) {
+          let handled = false
+          for (const file of e.dataTransfer.files) {
+            logger.info('文件类型', file.type)
+            if (accept === '*' || accept.split(',').some((a) => file.type.startsWith(a))) {
+              uploadFile(file, !handled) // Dont await these, any order is fine, only update on first one
+              handled = true
+            }
+            else {
+              logger.info(`require ${accept}, but got ${file.type}`)
+            }
+          }
+          return handled
+        }
+
+        node.onRemoved = function (e) {
+          fileInput.remove()
+        }
+
+        // TODO: 目前不兼容, 详情查看pasteFiles事件的来源
+        node.pasteFile = function (file) {
+          return false
+        }
+
+        return { widget: uploadWidget }
+      },
+    }
+  },
 })
 
 // 加载文件
@@ -46,6 +187,33 @@ app.registerExtension({
   async beforeRegisterNodeDef(nodeType, nodeData, app) {
     if (nodeData.name !== 'LoadFile') {
       return
+    }
+    /**
+     * 可以针对该节点定义的输入进行修改，如修改（增删）其widget类型，以及参数
+     * 但是此处拿不到其widget的输入，如果要做应该在widget内部是实现，传递回调进去
+     */
+    if (!nodeData.input) {
+      nodeData.input = {}
+    }
+    if (!nodeData.input.required) {
+      nodeData.input.required = {}
+    }
+    if (!nodeData.input.optional) {
+      nodeData.input.optional = {}
+    }
+    const inputs = { ...nodeData.input.optional, ...nodeData.input.required }
+    Object.keys(inputs).forEach((key) => {
+      const item = inputs[key]
+      // [0]为可选项或输入类型，[1]为参数
+      const attrs = item[1]
+      if (attrs?.file_upload) {
+        // 如果有参数file_upload则新增一个上传组件的输入
+        nodeData.input.optional[`${key}_upload`] = ['FILEUPLOAD', { widget: key, accept: attrs.accept }]
+      }
+    })
+
+    nodeType.prototype.onAdded = function (graph) {
+      this.setSize([300, 100])
     }
   },
 })
@@ -58,8 +226,6 @@ app.registerExtension({
     }
     const root = document.createElement('div')
     root.id = 'WorkUtils'
-    let count = 0
-
     nodeType.prototype.onAdded = function (graph) {
       const widget = this.addDOMWidget('$$preview_table', 'textoutput', root, {
         hideOnZoom: false,
@@ -69,15 +235,15 @@ app.registerExtension({
       widget.serializeValue = () => undefined
       // 设置初始宽高
       this.setSize([420, 300])
-      globalThis.createTable('#WorkUtils')
-      console.log('render123==========', this, widget)
+      globalThis.createTable(`#${root.id}`)
+      logger.debug('render', this, widget)
     }
 
     const onExecutedOriginal = nodeType.prototype.onExecuted
     nodeType.prototype.onExecuted = function (data) {
       onExecutedOriginal?.apply(this, arguments)
-      console.log('data', data)
-      console.log('node', this)
+      logger.info('data', data)
+      logger.info('node', this)
       // todo: 更新ui界面
     }
   },
